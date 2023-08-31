bbbcf5a5d23f8f87c605c20a3e5f031b52abb6a6
Rian Mendes <rian._santos@hottmail.com>
2023-08-31 19:28:51 -0300

    merge da master

--- src/main/java/com/sistema/gerenciador/clube/model/CadastroFuncionarioModel.java
+++ src/main/java/com/sistema/gerenciador/clube/model/CadastroFuncionarioModel.java
@@ -8,15 +8,20 @@ public class CadastroFuncionarioModel extends Conector {
 
     public Boolean salvar(Usuario usuario) {
         StringBuilder sb = new StringBuilder();
-        sb.append("INSERT INTO USUARIO (NOME, LOGIN, SENHA) \n");
-        sb.append("VALUES (':NOME', ':LOGIN', ':SENHA')");
+        sb.append("INSERT INTO USUARIO (NOME, ENDERECO, LOGIN, SENHA, TELEFONE, EMAIL, CARGO, SALARIO) \n");
+        sb.append("VALUES (':NOME', ':ENDERECO', ':LOGIN', ':SENHA', ':TELEFONE', ':EMAIL', ':CARGO', ':SALARIO')");
 
         String query = sb.toString();
 
         query = query.replace(":NOME", usuario.getNome());
+        query = query.replace(":ENDERECO", usuario.getEndereco());
         query = query.replace(":LOGIN", usuario.getLogin());
         query = query.replace(":SENHA", usuario.getSenha());
-        
+        query = query.replace(":TELEFONE", usuario.getTelefone());
+        query = query.replace(":EMAIL", usuario.getEmail());
+        query = query.replace(":CARGO", usuario.getCargo());
+        String salarioDoubleToString = String.valueOf(usuario.getSalario());
+        query = query.replace(":SALARIO", salarioDoubleToString);
         return persist(query);
     }
 }
